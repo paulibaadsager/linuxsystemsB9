e138fa8b729f626593de20e54b22e265b8069222
Pauli Baadsager <pauli.baadsager@icloud.com>
2021-03-04 20:33:18 +0000

    Add metods from python-kasa, and correct urls Relevant methods from python-kase implemented, and can be reached with http-requests (GET or POST) to appropriate urls.

--- tp-link/app.py
+++ tp-link/app.py
@@ -7,33 +7,65 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-p = SmartPlug("192.168.8.101")
-async def main():
+ip="192.168.8.101"
+p = SmartPlug(ip)
 
+#async def testing():
+#    await p.update()
+#    return p.alias
+##---
+async def get_alias():
     await p.update()
-    #print(p.alias)
     return p.alias
 
-async def mainto():
+async def get_state():
+    await p.update()
+    return p.state_information
+
+async def is_on():
+    await p.update()
+    return p.is_on
+
+async def turn_off():
     await p.turn_off()
 
+async def turn_on():
+    await p.turn_on()
 
-@app.route('/status')
-def status():
-    res=asyncio.run(main())
+@app.route('/info')
+def get_info():
+    alias=asyncio.run(get_alias())
+    powerStatus=asyncio.run(is_on())
+    return flask.jsonify({
+        'ip': ip,
+        'alias': alias,
+        'Power ON': powerStatus
+        })
+
+@app.route('/alias')
+def alias():
+    res=asyncio.run(get_alias())
     return flask.jsonify({'alias': res})
 
+@app.route('/state')
+def status():
+    res=asyncio.run(get_state())
+    return flask.jsonify(res)
+
+@app.route('/powerstatus')
+def powerstatus():
+    res=asyncio.run(is_on())
+    return flask.jsonify({'Power ON': res})
 
 @app.route('/deactivate', methods=['POST'])
 def turnoff():
-    asyncio.run(mainto())
-    return flask.jsonify({'msg':'slukket'})
+    asyncio.run(turn_off())
+    return flask.jsonify({'msg':'Device asked to turn off'})
 
-
-@app.route('/navn', methods=['POST'])
-def hurra():
-    print(request.get_json())
-    return flask.jsonify({'Du hedder:': 6})
+@app.route('/activate', methods=['POST'])
+def turnon():
+    asyncio.run(turn_on())
+    return flask.jsonify({'msg':'Device asked to turn on'})
 
 
 if __name__ == '__main__':
