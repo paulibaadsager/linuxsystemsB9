d776231a3dce9862264ebb9a4e26b61dc81b8bc8
Pauli Baadsager <pauli.baadsager@icloud.com>
2021-03-07 12:40:12 +0000

    Add IP-variable input to flask from url The ip address can be specified by the url, like: http://localhost:5000/info/192.168.8.102 Likewise for alias, activate and deactivate. There must be an ip.

--- tp-link/app.py
+++ tp-link/app.py
@@ -7,66 +7,70 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-ip="192.168.8.101"
-p = SmartPlug(ip)
+#ipm="192.168.8.101"
+#p = SmartPlug(ipm)
 
 #async def testing():
 #    await p.update()
 #    return p.alias
 ##---
-async def get_alias():
+async def get_alias(ip):
+    p = SmartPlug(ip)
     await p.update()
     return p.alias
 
-async def get_state():
+async def get_state(ip):
+    p = SmartPlug(ip)
     await p.update()
     return p.state_information
 
-async def is_on():
+async def is_on(ip):
+    p = SmartPlug(ip)
     await p.update()
     return p.is_on
 
-async def turn_off():
+async def turn_off(ip):
+    p = SmartPlug(ip)
     await p.turn_off()
 
-async def turn_on():
+async def turn_on(ip):
+    p = SmartPlug(ip)
     await p.turn_on()
 
-@app.route('/info')
-def get_info():
-    alias=asyncio.run(get_alias())
-    powerStatus=asyncio.run(is_on())
+@app.route('/info/<ip>')
+def get_info(ip):
+    alias=asyncio.run(get_alias(ip))
+    powerStatus=asyncio.run(is_on(ip))
     return flask.jsonify({
         'ip': ip,
         'alias': alias,
-        'Power ON': powerStatus
+        'Power on': powerStatus
         })
 
-@app.route('/alias')
-def alias():
-    res=asyncio.run(get_alias())
+@app.route('/alias/<ip>')
+def alias(ip):
+    res=asyncio.run(get_alias(ip))
     return flask.jsonify({'alias': res})
 
-@app.route('/state')
-def status():
-    res=asyncio.run(get_state())
+@app.route('/state/<ip>')
+def status(ip):
+    res=asyncio.run(get_state(ip))
     return flask.jsonify(res)
 
-@app.route('/powerstatus')
-def powerstatus():
-    res=asyncio.run(is_on())
-    return flask.jsonify({'Power ON': res})
+@app.route('/powerstatus/<ip>')
+def powerstatus(ip):
+    res=asyncio.run(is_on(ip))
+    return flask.jsonify({'Power on': res})
 
-@app.route('/deactivate', methods=['POST'])
-def turnoff():
-    asyncio.run(turn_off())
+@app.route('/deactivate/<ip>', methods=['POST'])
+def turnoff(ip):
+    asyncio.run(turn_off(ip))
     return flask.jsonify({'msg':'Device asked to turn off'})
 
-@app.route('/activate', methods=['POST'])
-def turnon():
-    asyncio.run(turn_on())
+@app.route('/activate/<ip>', methods=['POST'])
+def turnon(ip):
+    asyncio.run(turn_on(ip))
     return flask.jsonify({'msg':'Device asked to turn on'})
-
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
